9d96131c24cc48ef82b4edbf6386f17c3ddb64f6
zyrill <zyrill@users.noreply.github.com>
2017-09-07 19:26:42 +0200

    Changed detection algorithm
    Changed detection algorithm for ads: just remove all the muck before the first article.

--- golemAdBlock.user.js
+++ golemAdBlock.user.js
@@ -8,23 +8,14 @@
 // @description Remove Golem Ads
 // @grant       none
 // @run-at      document-idle
-// @version     1.0
+// @version     1.1.0
 // ==/UserScript==
 
-// Get all child elements of main presentation element
-var nodeList = document.getElementById("screen").childNodes;
-nodeList.forEach(
-  function(value, key, listObj, argument) {
-    var serializedHTML = value.outerHTML;
-    // Only consider those elements containing text
-    if ((typeof serializedHTML) === "string" ) {
-      //console.log(serializedHTML.search(">Anzeige</div>"));
-      // Select the element that says "Anzeige" and remove it
-      if (serializedHTML.search("style=\"text-align: left;\">Anzeige</div>") > 0) {
-        //console.log(serializedHTML);
-        //console.log(value);
-        value.remove();
-      }
-    }
-  }
-)
+try { var nodeList = document.getElementById("grandwrapper").childNodes; }
+catch(err) { console.log(err.message); }
+
+nodeList.every(function(element, index) {
+  element.remove;
+  if (element.id === "screen") return false
+  else return true
+})
