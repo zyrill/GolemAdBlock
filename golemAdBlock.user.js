ed05efc844db57dffb8a10fb2eda7137203a2c9c
pkleinejaeger <zyrill@users.noreply.github.com>
2018-05-28 15:52:12 +0200

    Update description

--- golemAdBlock.user.js
+++ golemAdBlock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author      zyrill
 // @copyright   2017, zyrill (https://openuserjs.org/users/zyrill)
-// @description Blocks anti-adblock notices and some ads that Adblock Plus cannot catch on golem.de
+// @description Blocks anti-adblock notices and some ads that Adblock Plus does not easily catch on golem.de
 // @encoding    utf-8
 // @grant       none
 // @include     https://www.golem.de/*
@@ -11,7 +11,7 @@
 // @namespace   https://github.com/zyrill/GolemAdBlock
 // @run-at      document-idle
 // @updateURL   https://openuserjs.org/meta/zyrill/Golem_Anti-Adblocker-Blocker.meta.js
-// @version     2.1.0
+// @version     2.1.1
 // ==/UserScript==
 var target = document.body;
 var observer = new MutationObserver(function(mutations) {
